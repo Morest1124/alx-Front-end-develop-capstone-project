9c52ef4e1176a031d3278b1ae3853881c6cb40fd
Morest1124 <morestburawa05@gmail.com>
2026-01-07 00:43:36 +0200

    Enable the remote server

--- BINARYBLADE24/src/api.js
+++ BINARYBLADE24/src/api.js
@@ -16,11 +16,11 @@ const getApiUrl = () => {
   const hostname = window.location.hostname;
   if (hostname === 'localhost' || hostname === '127.0.0.1') {
     // Point to local development backend when running locally
-    return "https://binaryblade2411.pythonanywhere.com/api/";
+    // return "http://localhost:8000/api/";
   }
   
   // 3. Fallback to production URL for deployed environment
-  // return "http://localhost:8000/api/";
+  return "https://binaryblade2411.pythonanywhere.com/api/";
 };
 
 const apiClient = axios.create({
